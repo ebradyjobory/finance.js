3458a2cabf9478654a681d353e84a4dfa238bed4
Essam Al Joubori <essam.joubori@gmail.com>
2014-11-30 22:20:10 -0500

    npv complete

--- finance.js
+++ finance.js
@@ -1,9 +1,20 @@
-var _$ = {};
+var $$ = {};
 
 (function(){
 
-// Net Present Value (NPV)
-
-
+  // Net Present Value (NPV)
+  $$NPV = function (r, cf0) {
+    var rate = r/100;
+    var npv = 0;
+    var seg = [cf0];
+    for (var i = 2; i < arguments.length; i++){
+      seg.push(arguments[i] / Math.pow((1 + rate), i - 1));
+    }
+    for (var j = 0; j < seg.length; j++) {
+      npv += seg[j];
+    }
+    return Math.round(npv * 100) / 100
+  };
 
 }).call(this);
+
