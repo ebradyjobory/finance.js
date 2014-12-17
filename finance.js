dc223c0b379c6de426f25c1a8cd20f73a1971f85
Essam Al Joubori <essam.joubori@gmail.com>
2014-12-17 11:06:25 -0500

    implement IRR and PP

--- finance.js
+++ finance.js
@@ -17,13 +17,55 @@ Finance.prototype.FV = function (rate, cf0, numOfPeriod) {
 };
 
 // Net Present Value (NPV)
-Finance.prototype.NPV = function (rate, cf0) {
-  var rate = rate/100, npv = cf0;
-  for (var i = 2; i < arguments.length; i++){
+Finance.prototype.NPV = function (rate) {
+  var rate = rate/100, npv = arguments[1];
+  for (var i = 2; i < arguments.length; i++) {
     npv +=(arguments[i] / Math.pow((1 + rate), i - 1));
   }
   return Math.round(npv * 100) / 100;
 };
+
+// Internal Rate of Return (IRR)
+Finance.prototype.IRR = function(cfs) {  
+  var bestGuess;
+  var checkNPV = function(rate, arguments){
+    var currentNPV;
+    // base case
+    // TODO: to make the IRR function take more than 4 arguments
+    currentNPV = Finance.prototype.NPV(rate, arguments[0], arguments[1], arguments[2], arguments[3]);
+    console.log(currentNPV);
+    if (currentNPV <= 0) {
+      bestGuess = rate;
+      return;
+    } 
+    checkNPV(rate+= 0.01, arguments);
+  }; 
+  checkNPV(0.01, arguments);
+  return Math.round(bestGuess * 100) / 100;
+};
+
+// Payback Period (PP) = Cost of Project / Annual Cash Inflows
+Finance.prototype.PP = function(numOfYears, cfs) {
+  // for even cash flows
+  if (numOfYears === 0) {
+    return arguments[1] / arguments[2];
+  }
+  // for uneven cash flows
+  //var cashFlow = arguments[1];
+  var CumulativeCashFlow = arguments[1];
+  var yearsCounter = 1;  
+  for (i = 2; i < arguments.length; i++) {
+    CumulativeCashFlow += arguments[i];
+    if (CumulativeCashFlow > 0) {
+      yearsCounter += (CumulativeCashFlow - arguments[i]) / arguments[i];
+      return yearsCounter;
+    } else {
+      yearsCounter++;
+    }
+  }
+    
+};
+
 
 
 
--- specs.js
+++ specs.js
@@ -1,4 +1,5 @@
 var assert = chai.assert;
+var expect = chai.expect;
 
 var cal = new Finance();
 
@@ -14,6 +15,20 @@ describe('FinanceJS', function() {
 
   it('should compute NPV', function() {
     assert.equal(cal.NPV(10, -500000, 200000, 300000, 200000), 80015.03);
+  });
+
+  it('should compute IRR', function() {
+    var irr = cal.IRR(-123400, 36200, 54800, 48100);
+    expect(irr).to.be.within(5.90, 6.00); // should be 5.96
+  });
+  
+  it('should compute PP for even cash flows', function() {
+    assert.equal(cal.PP(0, 105, 25), 4.2);
+  });
+
+  it('should compute PP for uneven cash flows', function() {
+    var pp = cal.PP(5, -50, 10, 13, 16, 19, 22);
+    expect(pp).to.be.within(3.3, 3.6);
   });
 
 
