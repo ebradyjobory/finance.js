c9455e81193445cd27ed470bc0070ef23f2b2f87
Igor Santos <igorsantos07@gmail.com>
2017-01-19 23:53:24 -0200

    Simplifying modular usage of finance.js, dispensing the main "class"

--- finance.js
+++ finance.js
@@ -2,25 +2,25 @@
 //For more information, visit http://financejs.org
 //Copyright 2014 - 2015 Essam Al Joubori, MIT license
 
-// Instantiate a Finance class
-var Finance = function() {};
+// Instantiate the main object
+var Finance = {};
 
 // Present Value (PV)
-Finance.prototype.PV = function (rate, cf1) {
+Finance.PV = function (rate, cf1) {
   var rate = rate/100, pv;
   pv = cf1 / (1 + rate);
   return Math.round(pv * 100) / 100;
 };
 
 // Future Value (FV)
-Finance.prototype.FV = function (rate, cf0, numOfPeriod) {
+Finance.FV = function (rate, cf0, numOfPeriod) {
   var rate = rate/100, fv;
   fv = cf0 * Math.pow((1 + rate), numOfPeriod);
   return Math.round(fv * 100) / 100;
 };
 
 // Net Present Value (NPV)
-Finance.prototype.NPV = function (rate) {
+Finance.NPV = function (rate) {
   var rate = rate/100, npv = arguments[1];
   for (var i = 2; i < arguments.length; i++) {
     npv +=(arguments[i] / Math.pow((1 + rate), i - 1));
@@ -41,7 +41,7 @@ function seekZero(fn) {
 }
 
 // Internal Rate of Return (IRR)
-Finance.prototype.IRR = function(cfs) {
+Finance.IRR = function(cfs) {
   var args = arguments;
   var numberOfTries = 1;
   // Cash flow values must contain at least one positive value and one negative value
@@ -67,7 +67,7 @@ Finance.prototype.IRR = function(cfs) {
 };
 
 // Payback Period (PP)
-Finance.prototype.PP = function(numOfPeriods, cfs) {
+Finance.PP = function(numOfPeriods, cfs) {
   // for even cash flows
   if (numOfPeriods === 0) {
     return Math.abs(arguments[1]) / arguments[2];
@@ -81,19 +81,19 @@ Finance.prototype.PP = function(numOfPeriods, cfs) {
       yearsCounter += (cumulativeCashFlow - arguments[i]) / arguments[i];
       return yearsCounter;
     } else {
-      yearsCounter++;
+      ++yearsCounter;
     }
   }
 };
 
 // Return on Investment (ROI)
-Finance.prototype.ROI = function(cf0, earnings) {
+Finance.ROI = function(cf0, earnings) {
   var roi = (earnings - Math.abs(cf0)) / Math.abs(cf0) * 100;
   return Math.round(roi * 100) / 100;
 };
 
 // Amortization
-Finance.prototype.AM = function (principal, rate, period, yearOrMonth, payAtBeginning) {
+Finance.AM = function (principal, rate, period, yearOrMonth, payAtBeginning) {
   var numerator, denominator, am;
   var ratePerPeriod = rate / 12 / 100;
 
@@ -113,8 +113,8 @@ Finance.prototype.AM = function (principal, rate, period, yearOrMonth, payAtBegi
   am = principal * (numerator / denominator);
   return Math.round(am * 100) / 100;
 
-  function buildNumerator(numInterestAccruals){
-    if( payAtBeginning ){
+  function buildNumerator(numInterestAccruals) {
+    if (payAtBeginning) {
       //if payments are made in the beginning of the period, then interest shouldn't be calculated for first period
       numInterestAccruals -= 1;
     }
@@ -123,7 +123,7 @@ Finance.prototype.AM = function (principal, rate, period, yearOrMonth, payAtBegi
 };
 
 // Profitability Index (PI)
-Finance.prototype.PI = function(rate, cfs){
+Finance.PI = function(rate, cfs){
   var totalOfPVs = 0, PI;
   for (var i = 2; i < arguments.length; i++) {
     var discountFactor;
@@ -136,7 +136,7 @@ Finance.prototype.PI = function(rate, cfs){
 };
 
 // Discount Factor (DF)
-Finance.prototype.DF = function(rate, numOfPeriods) {
+Finance.DF = function(rate, numOfPeriods) {
   var dfs = [], discountFactor;
   for (var i = 1; i < numOfPeriods; i++) {
     discountFactor = 1 / Math.pow((1 + rate/100), (i - 1));
@@ -147,52 +147,50 @@ Finance.prototype.DF = function(rate, numOfPeriods) {
 };
 
 // Compound Interest (CI)
-Finance.prototype.CI = function(rate, numOfCompoundings, principal, numOfPeriods) {
+Finance.CI = function(rate, numOfCompoundings, principal, numOfPeriods) {
   var CI = principal * Math.pow((1 + ((rate/100)/ numOfCompoundings)), numOfCompoundings * numOfPeriods);
   return Math.round(CI * 100) / 100;
 };
 
 // Compound Annual Growth Rate (CAGR)
-Finance.prototype.CAGR = function(beginningValue, endingValue, numOfPeriods) {
+Finance.CAGR = function(beginningValue, endingValue, numOfPeriods) {
   var CAGR = Math.pow((endingValue / beginningValue), 1 / numOfPeriods) - 1;
   return Math.round(CAGR * 10000) / 100;
 };
 
 // Leverage Ratio (LR)
-Finance.prototype.LR = function(totalLiabilities, totalDebts, totalIncome) {
+Finance.LR = function(totalLiabilities, totalDebts, totalIncome) {
   return (totalLiabilities  + totalDebts) / totalIncome;
 };
 
 // Rule of 72
-Finance.prototype.R72 = function(rate) {
+Finance.R72 = function(rate) {
   return 72 / rate;
 };
 
 // Weighted Average Cost of Capital (WACC)
-Finance.prototype.WACC = function(marketValueOfEquity, marketValueOfDebt, costOfEquity, costOfDebt, taxRate) {
-  E = marketValueOfEquity;
-  D = marketValueOfDebt;
-  V =  marketValueOfEquity + marketValueOfDebt;
-  Re = costOfEquity;
-  Rd = costOfDebt;
-  T = taxRate;
+Finance.WACC = function(marketValueOfEquity, marketValueOfDebt, costOfEquity, costOfDebt, taxRate) {
+  var E = marketValueOfEquity;
+  var D = marketValueOfDebt;
+  var V =  marketValueOfEquity + marketValueOfDebt;
+  var Re = costOfEquity;
+  var Rd = costOfDebt;
+  var T = taxRate;
 
   var WACC = ((E / V) * Re/100) + (((D / V) * Rd/100) * (1 - T/100));
   return Math.round(WACC * 1000) / 10;
 };
 
 // PMT calculates the payment for a loan based on constant payments and a constant interest rate
-Finance.prototype.PMT = function(fractionalRate, numOfPayments, principal) {
+Finance.PMT = function(fractionalRate, numOfPayments, principal) {
   return -principal * fractionalRate/(1-Math.pow(1+fractionalRate,-numOfPayments))
 };
 
 // IAR calculates the Inflation-adjusted return
-Finance.prototype.IAR = function(investmentReturn, inflationRate){
+Finance.IAR = function(investmentReturn, inflationRate){
   return 100 * (((1 + investmentReturn) / (1 + inflationRate)) - 1);
 }
 
-if (typeof exports !== 'undefined') {
-  if (typeof module !== 'undefined' && module.exports) {
-    module.exports = Finance;
-  }
+if (typeof exports !== 'undefined' && typeof module !== 'undefined' && module.exports) {
+  module.exports = Finance;
 }
