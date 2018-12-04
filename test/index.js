93fe8c7576426484bc087f789dc985223e45a917
Song Chuansheng <songchuansheng@91jinrong.com>
2018-12-04 15:36:55 +0800

    add RATE test

--- test/index.js
+++ test/index.js
@@ -120,6 +120,10 @@ describe('FinanceJS', function() {
     cal.IAR(0.08, 0.03).should.equal(4.854368932038833);
   });
 
+  it('should compute RATE', function() {
+    Number(cal.RATE(12, -933.33, 10000, 0, 0).toFixed(5)).should.equal(0.01788);
+  });
+
   it('should compute XIRR', function() {
     cal.XIRR([-1000, -100, 1200],[new Date(2015, 11, 1 ), new Date(2016, 7, 1 ), new Date(2016, 7, 19 )],0 ).should.equal(14.11);
   });
