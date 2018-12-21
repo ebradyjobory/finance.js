e47c1cc254288bc368da7d56d2ef368372eebe6a
Olamilekan Odukoya <olarpringo@gmail.com>
2018-12-21 20:02:09 +0100

    linted code base

--- finance.js
+++ finance.js
@@ -1,269 +1,278 @@
-//Finance.js
-//For more information, visit http://financejs.org
-//Copyright 2014 - 2015 Essam Al Joubori, MIT license
+// Finance.js
+// For more information, visit http://financejs.org
+// Copyright 2014 - 2015 Essam Al Joubori, MIT license
 
 // Instantiate a Finance class
-var Finance = function() {};
+const Finance = function Finance() {};
 
 // Present Value (PV)
-Finance.prototype.PV = function (rate, cf1, numOfPeriod) {
-  numOfPeriod = typeof numOfPeriod !== 'undefined' ? numOfPeriod : 1;
-  var rate = rate/100, pv;
-  pv = cf1 / Math.pow((1 + rate),numOfPeriod);
+Finance.prototype.PV = function PV(rate, cf1, numOfPeriod) {
+  const newNumOfPeriod = typeof numOfPeriod !== 'undefined' ? numOfPeriod : 1;
+  const newRate = rate / 100;
+  const pv = cf1 / ((1 + newRate) ** newNumOfPeriod);
   return Math.round(pv * 100) / 100;
 };
 
 // Future Value (FV)
-Finance.prototype.FV = function (rate, cf0, numOfPeriod) {
-  var rate = rate/100, fv;
-  fv = cf0 * Math.pow((1 + rate), numOfPeriod);
+Finance.prototype.FV = function FV(rate, cf0, numOfPeriod) {
+  const newRate = rate / 100;
+  const fv = cf0 * ((1 + newRate) ** numOfPeriod);
   return Math.round(fv * 100) / 100;
 };
 
 // Net Present Value (NPV)
-Finance.prototype.NPV = function (rate) {
-  var rate = rate/100, npv = arguments[1];
-  for (var i = 2; i < arguments.length; i++) {
-    npv +=(arguments[i] / Math.pow((1 + rate), i - 1));
+Finance.prototype.NPV = function NPV(...args) {
+  const rate = args[0] / 100;
+  let npv = args[1];
+  for (let i = 2; i < args.length; i += 1) {
+    npv += (args[i] / ((1 + rate) ** (i - 1)));
   }
   return Math.round(npv * 100) / 100;
 };
 
-// seekZero seeks the zero point of the function fn(x), accurate to within x \pm 0.01. fn(x) must be decreasing with x.
+/**
+  seekZero seeks the zero point of the function fn(x),
+  accurate to within x \pm 0.01. fn(x) must be decreasing with x.
+* */
+
 function seekZero(fn) {
-  var x = 1;
+  let x = 1;
   while (fn(x) > 0) {
     x += 1;
   }
   while (fn(x) < 0) {
-    x -= 0.01
+    x -= 0.01;
   }
   return x + 0.01;
 }
 
 // Internal Rate of Return (IRR)
-Finance.prototype.IRR = function(cfs) {
-  var args = arguments;
-  var numberOfTries = 1;
+Finance.prototype.IRR = function IRR(...cfs) {
+  const args = cfs;
+  let numberOfTries = 1;
   // Cash flow values must contain at least one positive value and one negative value
-  var positive, negative;
-  Array.prototype.slice.call(args).forEach(function (value) {
+  let positive; let
+    negative;
+  Array.prototype.slice.call(args).forEach((value) => {
     if (value > 0) positive = true;
     if (value < 0) negative = true;
-  })
+  });
   if (!positive || !negative) throw new Error('IRR requires at least one positive value and one negative value');
   function npv(rate) {
-    numberOfTries++;
+    numberOfTries += 1;
     if (numberOfTries > 1000) {
       throw new Error('IRR can\'t find a result');
     }
-    var rrate = (1 + rate/100);
-    var npv = args[0];
-    for (var i = 1; i < args.length; i++) {
-      npv += (args[i] / Math.pow(rrate, i));
+    const rrate = (1 + rate / 100);
+    let newNpv = args[0];
+    for (let i = 1; i < args.length; i += 1) {
+      newNpv += (args[i] / (rrate ** i));
     }
-    return npv;
+    return newNpv;
   }
   return Math.round(seekZero(npv) * 100) / 100;
 };
 
 // Payback Period (PP)
-Finance.prototype.PP = function(numOfPeriods, cfs) {
+Finance.prototype.PP = function PP(...args) {
   // for even cash flows
-  if (numOfPeriods === 0) {
-    return Math.abs(arguments[1]) / arguments[2];
+  if (args[0] === 0) {
+    return Math.abs(args[1]) / args[2];
   }
   // for uneven cash flows
-  var cumulativeCashFlow = arguments[1];
-  var yearsCounter = 1;
-  for (i = 2; i < arguments.length; i++) {
-    cumulativeCashFlow += arguments[i];
+  let cumulativeCashFlow = args[1];
+  let yearsCounter = 1;
+  for (let i = 2; i < args.length; i += 1) {
+    cumulativeCashFlow += args[i];
     if (cumulativeCashFlow > 0) {
-      yearsCounter += (cumulativeCashFlow - arguments[i]) / arguments[i];
+      yearsCounter += (cumulativeCashFlow - args[i]) / args[i];
       return yearsCounter;
-    } else {
-      yearsCounter++;
     }
+    yearsCounter += 1;
   }
+  return yearsCounter;
 };
 
 // Return on Investment (ROI)
-Finance.prototype.ROI = function(cf0, earnings) {
-  var roi = (earnings - Math.abs(cf0)) / Math.abs(cf0) * 100;
+Finance.prototype.ROI = function ROI(cf0, earnings) {
+  const roi = (earnings - Math.abs(cf0)) / Math.abs(cf0) * 100;
   return Math.round(roi * 100) / 100;
 };
 
 // Amortization
-Finance.prototype.AM = function (principal, rate, period, yearOrMonth, payAtBeginning) {
-  var numerator, denominator, am;
-  var ratePerPeriod = rate / 12 / 100;
+function buildNumerator(numInterestAccruals, payAtBeginning, ratePerPeriod) {
+  let newNumInterestAccruals = numInterestAccruals;
+  if (payAtBeginning) {
+    /**
+      if payments are made in the beginning of the period,
+      then interest shouldn't be calculated for first period
+    * */
+    newNumInterestAccruals -= 1;
+  }
+  return (ratePerPeriod * ((1 + ratePerPeriod) ** newNumInterestAccruals));
+}
+
+Finance.prototype.AM = function AM(principal, rate, period, yearOrMonth, payAtBeginning) {
+  let numerator; let denominator;
+  const ratePerPeriod = rate / 12 / 100;
 
   // for inputs in years
   if (!yearOrMonth) {
-    numerator = buildNumerator(period * 12);
-    denominator = Math.pow((1 + ratePerPeriod), period * 12) - 1;
+    numerator = buildNumerator(period * 12, payAtBeginning, ratePerPeriod);
+    denominator = (((1 + ratePerPeriod) ** (period * 12)) - 1);
 
   // for inputs in months
   } else if (yearOrMonth === 1) {
-    numerator = buildNumerator(period)
-    denominator = Math.pow((1 + ratePerPeriod), period) - 1;
-
+    numerator = buildNumerator(period, payAtBeginning, ratePerPeriod);
+    denominator = (((1 + ratePerPeriod) ** period) - 1);
   } else {
-    console.log('not defined');
+    throw new Error('not defined');
   }
-  am = principal * (numerator / denominator);
+  const am = principal * (numerator / denominator);
   return Math.round(am * 100) / 100;
-
-  function buildNumerator(numInterestAccruals){
-    if( payAtBeginning ){
-      //if payments are made in the beginning of the period, then interest shouldn't be calculated for first period
-      numInterestAccruals -= 1;
-    }
-    return ratePerPeriod * Math.pow((1 + ratePerPeriod), numInterestAccruals);
-  }
 };
 
 // Profitability Index (PI)
-Finance.prototype.PI = function(rate, cfs){
-  var totalOfPVs = 0, PI;
-  for (var i = 2; i < arguments.length; i++) {
-    var discountFactor;
+Finance.prototype.PI = function PI(...args) {
+  let totalOfPVs = 0;
+  for (let i = 2; i < args.length; i += 1) {
     // calculate discount factor
-    discountFactor = 1 / Math.pow((1 + rate/100), (i - 1));
-    totalOfPVs += arguments[i] * discountFactor;
+    const discountFactor = 1 / ((1 + args[0] / 100) ** (i - 1));
+    totalOfPVs += args[i] * discountFactor;
   }
-  PI = totalOfPVs/Math.abs(arguments[1]);
-  return Math.round(PI * 100) / 100;
+  const newPI = totalOfPVs / Math.abs(args[1]);
+  return Math.round(newPI * 100) / 100;
 };
 
 // Discount Factor (DF)
-Finance.prototype.DF = function(rate, numOfPeriods) {
-  var dfs = [], discountFactor;
-  for (var i = 1; i < numOfPeriods; i++) {
-    discountFactor = 1 / Math.pow((1 + rate/100), (i - 1));
-    roundedDiscountFactor = Math.ceil(discountFactor * 1000)/1000;
+Finance.prototype.DF = function DF(rate, numOfPeriods) {
+  const dfs = []; let
+    discountFactor;
+  for (let i = 1; i < numOfPeriods; i += 1) {
+    discountFactor = 1 / ((1 + rate / 100) ** (i - 1));
+    const roundedDiscountFactor = Math.ceil(discountFactor * 1000) / 1000;
     dfs.push(roundedDiscountFactor);
   }
   return dfs;
 };
 
 // Compound Interest (CI)
-Finance.prototype.CI = function(rate, numOfCompoundings, principal, numOfPeriods) {
-  var CI = principal * Math.pow((1 + ((rate/100)/ numOfCompoundings)), numOfCompoundings * numOfPeriods);
-  return Math.round(CI * 100) / 100;
+Finance.prototype.CI = function CI(rate, numOfCompoundings, principal, numOfPeriods) {
+  const calc = ((1 + ((rate / 100) / numOfCompoundings)) ** (numOfCompoundings * numOfPeriods));
+  const newCI = principal * calc;
+  return Math.round(newCI * 100) / 100;
 };
 
 // Compound Annual Growth Rate (CAGR)
-Finance.prototype.CAGR = function(beginningValue, endingValue, numOfPeriods) {
-  var CAGR = Math.pow((endingValue / beginningValue), 1 / numOfPeriods) - 1;
-  return Math.round(CAGR * 10000) / 100;
+Finance.prototype.CAGR = function CAGR(beginningValue, endingValue, numOfPeriods) {
+  const newCAGR = ((endingValue / beginningValue) ** (1 / numOfPeriods)) - 1;
+  return Math.round(newCAGR * 10000) / 100;
 };
 
 // Leverage Ratio (LR)
-Finance.prototype.LR = function(totalLiabilities, totalDebts, totalIncome) {
-  return (totalLiabilities  + totalDebts) / totalIncome;
+Finance.prototype.LR = function LR(totalLiabilities, totalDebts, totalIncome) {
+  return (totalLiabilities + totalDebts) / totalIncome;
 };
 
 // Rule of 72
-Finance.prototype.R72 = function(rate) {
+Finance.prototype.R72 = function R72(rate) {
   return 72 / rate;
 };
 
 // Weighted Average Cost of Capital (WACC)
-Finance.prototype.WACC = function(marketValueOfEquity, marketValueOfDebt, costOfEquity, costOfDebt, taxRate) {
-  E = marketValueOfEquity;
-  D = marketValueOfDebt;
-  V =  marketValueOfEquity + marketValueOfDebt;
-  Re = costOfEquity;
-  Rd = costOfDebt;
-  T = taxRate;
+Finance.prototype.WACC = function WACC(mktValOfEqui, mktValOfDebt, ke, kd, tRate) {
+  const E = mktValOfEqui;
+  const D = mktValOfDebt;
+  const V = mktValOfEqui + mktValOfDebt;
+  const Re = ke;
+  const Rd = kd;
+  const T = tRate;
 
-  var WACC = ((E / V) * Re/100) + (((D / V) * Rd/100) * (1 - T/100));
-  return Math.round(WACC * 1000) / 10;
+  const newWACC = ((E / V) * Re / 100) + (((D / V) * Rd / 100) * (1 - T / 100));
+  return Math.round(newWACC * 1000) / 10;
 };
 
 // PMT calculates the payment for a loan based on constant payments and a constant interest rate
-Finance.prototype.PMT = function(fractionalRate, numOfPayments, principal) {
-  return -principal * fractionalRate/(1-Math.pow(1+fractionalRate,-numOfPayments))
+Finance.prototype.PMT = function PMT(fractionalRate, numOfPayments, principal) {
+  return -principal * fractionalRate / (1 - ((1 + fractionalRate) ** -numOfPayments));
 };
 
 // IAR calculates the Inflation-adjusted return
-Finance.prototype.IAR = function(investmentReturn, inflationRate){
+Finance.prototype.IAR = function IAR(investmentReturn, inflationRate) {
   return 100 * (((1 + investmentReturn) / (1 + inflationRate)) - 1);
+};
+
+// Returns duration in years between two dates
+function durYear(first, last) {
+  return (Math.abs(last.getTime() - first.getTime()) / (1000 * 3600 * 24 * 365));
+}
+
+// Returns Sum of f(x)/f'(x)
+function sumEq(cfs, durs, guess) {
+  let sumFx = 0;
+  let sumFdx = 0;
+  for (let i = 0; i < cfs.length; i += 1) {
+    sumFx += cfs[i] / ((1 + guess) ** durs[i]);
+  }
+  for (let i = 0; i < cfs.length; i += 1) {
+    sumFdx += (-cfs[i] * durs[i]) * ((1 + guess) ** (-1 - durs[i]));
+  }
+  return sumFx / sumFdx;
 }
 
 // XIRR - IRR for irregular intervals
-Finance.prototype.XIRR = function(cfs, dts, guess) {
-if (cfs.length != dts.length) throw new Error('Number of cash flows and dates should match');
+Finance.prototype.XIRR = function XIRR(cfs, dts, guess) {
+  if (cfs.length !== dts.length) throw new Error('Number of cash flows and dates should match');
 
-  var positive, negative;
-  Array.prototype.slice.call(cfs).forEach(function (value) {
+  let positive; let
+    negative;
+  Array.prototype.slice.call(cfs).forEach((value) => {
     if (value > 0) positive = true;
     if (value < 0) negative = true;
   });
 
   if (!positive || !negative) throw new Error('XIRR requires at least one positive value and one negative value');
-  
 
-  guess = !!guess ? guess : 0;
 
-  var limit = 100; //loop limit
-  var guess_last;
-  var durs = [];
+  let newGuess = guess || 0;
+
+  let limit = 100; // loop limit
+  let guessLast;
+  const durs = [];
 
   durs.push(0);
 
-  //Create Array of durations from First date
-  for(var i = 1; i < dts.length; i++) {
+  // Create Array of durations from First date
+  for (let i = 1; i < dts.length; i += 1) {
     durs.push(durYear(dts[0], dts[i]));
   }
-  
-  do {
-    guess_last = guess;
-    guess = guess_last - sumEq(cfs, durs, guess_last);
-    limit--;
-    
-  }while(guess_last.toFixed(5) != guess.toFixed(5) && limit > 0);
 
-  var xirr = guess_last.toFixed(5) != guess.toFixed(5) ? null : guess*100;
+  do {
+    guessLast = newGuess;
+    newGuess = guessLast - sumEq(cfs, durs, guessLast);
+    limit -= 1;
+  } while (guessLast.toFixed(5) !== newGuess.toFixed(5) && limit > 0);
+
+  const xirr = guessLast.toFixed(5) !== newGuess.toFixed(5) ? null : newGuess * 100;
 
   return Math.round(xirr * 100) / 100;
-}
+};
 
-//CAPM calculates expected return of an asset.
-Finance.prototype.CAPM = function (rf, beta, emr, err) {
-  var ans = rf/100 + beta * (emr/100 - rf/100);
+// CAPM calculates expected return of an asset.
+Finance.prototype.CAPM = function CAPM(rf, beta, emr) {
+  const ans = rf / 100 + beta * (emr / 100 - rf / 100);
   return ans;
-}
+};
 
-//Returns the Value of stock with dividend growing at a constant growth rate to perpetuity.
-Finance.prototype.stockPV = function (g, ke, D0) {
-  var valueOfStock = (D0 * (1 + g/100))/((ke/100) - (g/100))
-  return Math.round(valueOfStock)
-}
-
-//Returns Sum of f(x)/f'(x)
-function sumEq(cfs, durs, guess) {
-  var sum_fx = 0;
-  var sum_fdx = 0;
-  for (var i = 0; i < cfs.length; i++) {
-    sum_fx = sum_fx + (cfs[i] / Math.pow(1 + guess, durs[i]));
-  }
-  for ( i = 0; i < cfs.length; i++) {
-    sum_fdx = sum_fdx + (-cfs[i] * durs[i] * Math.pow(1 + guess, -1 - durs[i]));
-  }
-  return sum_fx / sum_fdx;
-}
-
-//Returns duration in years between two dates
-function durYear(first, last) {
-  return (Math.abs(last.getTime() - first.getTime()) / (1000 * 3600 * 24 * 365));
-}
-
+// Returns the Value of stock with dividend growing at a constant growth rate to perpetuity.
+Finance.prototype.stockPV = function stockPV(g, ke, D0) {
+  const valueOfStock = (D0 * (1 + g / 100)) / ((ke / 100) - (g / 100));
+  return Math.round(valueOfStock);
+};
 
 if (typeof exports !== 'undefined') {
   if (typeof module !== 'undefined' && module.exports) {
-      module.exports = Finance;
-      module.exports.Finance = Finance;
+    module.exports = Finance;
+    module.exports.Finance = Finance;
   }
 }
